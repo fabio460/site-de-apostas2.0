31da9d28fc4512bdb922c830780e0931aedf3ff5
unknown <dasilvadeoliveirafabio@gmail.com>
2023-07-31 16:53:01 -0300

    teste 4

--- src/app/components/tabelaSimples/index.tsx
+++ src/app/components/tabelaSimples/index.tsx
@@ -8,7 +8,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
-import "./tabela.css"
+
 
 type selectedType={
   id:string,
